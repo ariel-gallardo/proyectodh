cdf150126d51977aefb3a431df1cd8fa23644f48
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-05-23 00:18:19 -0300

    mejora responsive - restructura de footer

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -10,68 +10,59 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark pl-0">
-        <div class="d-flex">
-            <div class="d-flex">
-                <a class="NavLogo navbar-brand mx-0 nav-link mt-3 pl-1" href="#">
-                    <h3>S.M.<img src="/img/nav/super.svg" width="40vw" alt="buy"></h3>
+    <nav class="bg-dark navbar navbar-expand-md navbar-dark p-md-1">
+                <a class="navbar-brand m-0 p-0 nav-link" href="#">
+                    <h3>S.M.<img src="/img/nav/super.svg" width="40vw" alt="SuperMarket"></h3>
                     <h6>Digital House</h6>
                 </a>
-                <ul class="nav">
-                    <li class="nav-item">
-                        <!-- <a class = "position-relative" href="#"> -->
-                        <div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog"
-                            aria-labelledby="myModalLabel" aria-hidden="true">
+                <ul class="list-inline">
+                    <li class="list-inline-item m-0">
+                        <div class="modal fade" id="Usuario" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+                            aria-hidden="true">
                             <div class="modal-dialog cascading-modal" role="document">
                                 <div class="modal-content">
                                     <?php /* require_once "usuario/index.php"; */?>
                                 </div>
                             </div>
                         </div>
-                        <div class="text-center">
-                            <a href="" class="btn btn-default btn-rounded mb-4" data-toggle="modal"
-                                data-target="#modalLoginForm">
-                                <img src="/img/nav/usuario.svg" alt="Perfil" width="40vw">
-                            </a>
+                        <div class="text-center" class="btn btn-default btn-rounded" data-toggle="modal"
+                            data-target="#Usuario">
+                            <img src="/img/nav/usuario.svg" alt="Perfil" width="35vw">
                         </div>
-                        <!-- <pre class = "position-absolute py-2 text-white">Bienvenido</pre> -->
                     </li>
-                    <li class="nav-item mr-3 rounded position-relative">
-                        <a href="#">
-                            <img src="/img/nav/carro.svg" alt="Carro" width="40vw">
-                            <span class="badge badge-primary position-absolute mt-4">0</span>
-                        </a>
+                    <li class="list-inline-item position-relative btn btn-default btn-rounded m-0 p-0 pr-4" data-toggle="modal"
+                        data-target="#Usuario">
+                        <img src="/img/nav/carro.svg" alt="Carro" width="35vw">
+                        <span class="badge badge-primary position-absolute mt-4">20</span>
                     </li>
-                    <li class="nav-item rounded position-relative">
-                        <a href="#">
-                            <img src="/img/nav/notificacion.svg" alt="Carro" width="40vw">
-                            <span class="badge badge-primary position-absolute mt-4">0</span>
-                        </a>
+                    <li class="list-inline-item position-relative btn btn-default btn-rounded m-0 p-0 pr-3" data-toggle="modal"
+                        data-target="#Usuario">
+                        <img src="/img/nav/notificacion.svg" alt="Carro" width="35vw">
+                        <span class="badge badge-primary position-absolute mt-4">20</span>
                     </li>
                 </ul>
                 <!-- Boton Menu Acortable -->
-                <button class="px-0 ml-4 mb-5 navbar-toggler border-0" type="button" data-toggle="collapse"
+                <button class="navbar-toggler border-0 margin-0 p-1" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-            </div>
             <!-- Lista Menu Acortable -->
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav pl-md-2 mr-auto ml-lg-5">
+                <ul class="navbar-nav text-center pl-md-1 mr-auto ml-lg-5 ml-md-2">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home</a>
+                        <a class="nav-link m-md-0 p-md-1" href="#">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">F.A.Q.</a>
+                        <a class="nav-link m-md-0 p-md-1" href="#">F.A.Q.</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Contacto</a>
+                        <a class="nav-link m-md-0 p-md-1" href="#">Contacto</a>
                     </li>
                 </ul>
             </div>
             <!-- Buscador -->
-            <div class="NavBuscador container col-10 mt-3 pb-md-5 mt-md-0 col-md-5 col-lg-5 mr-lg-0 d-inline">
+            <div class="container d-inline col-9 mt-1 pb-md-0 col-md-5 pb-lg-0  text-center">
                 <form class="row border rounded  shadow-lg" action="index.html" method="get">
                     <!--Menu Selector-->
                     <div class="dropdown col-5 col-md-4 col-lg-3 px-md-0">
@@ -79,7 +70,7 @@
                             aria-haspopup="true" aria-expanded="false">
                             Categoria
                         </a>
-                        <div class="dropdown-menu text-center my-3" aria-labelledby="navbarDropdown">
+                        <div class="dropdown-menu my-3" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="#">Indumentaria</a>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="#">Ferreteria</a>
@@ -89,12 +80,11 @@
                     <input class="border col-5 col-md-6 col-lg-8 px-md-0 text-center" type="text" name="articulo"
                         placeholder=" Articulo ID/Nombre">
                     <button class="btn col-1 py-0" type="submit"><img src="/img/nav/buscar.svg" alt="Lupa"
-                            width="25vw"></button>
+                            width="20vw"></button>
                 </form>
             </div>
-        </div>
     </nav>
-    <header class="__wrapper-carousel">
+    <header>
         <div id="carouselExampleControls" class="carousel slide carousel-fade" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active slide1">
@@ -136,30 +126,25 @@
     <main class="container">
     </main>
     <footer>
-        <section class="__wrapper-contact">
-            <div class="row __max-width px-md-4">
-                <article class="col-12 text-center col-sm-6 text-sm-left text-md-left col-lg-3">
-                    <h4>Locales</h4>
-                    <p><strong>Moreno 567</strong>, Mendoza<br>+54 263 456785</p>
-                </article>
-                <article class="col-12 text-center col-sm-6 text-sm-left text-md-left col-lg-3">
-                    <p><strong>San Martin</strong>, Mendoza<br>+54 263 4567543</p>
-                </article>
-                <article class="col-12 text-center col-sm-6 text-sm-left text-md-left col-lg-3">
-                    <h4>Horarios</h4>
+        <section class="text-center">
+            <h4>Sucursales</h4>
+            <ul class="list-inline">
+                <li class="list-inline-item mx-5 my-3 border rounded p-2">
+                    <p><strong>Moreno 567</strong>, Mendoza</p>
+                    <a class="btn" href="tel:2610001000"><img src="/img/footer/whatsapp.svg" alt="Telefono"
+                            width="30vw"><b>261 010-1010</b></a>
                     <p><strong>Lunes a Jueves</strong><br>17 a 22h</p>
-                </article>
-                <article class="col-12 text-center col-sm-6 text-sm-left text-md-left col-lg-3">
+                </li>
+                <li class="list-inline-item mx-5 my-3 border rounded p-2">
+                    <p><strong>San Martin 342</strong>, Mendoza</p>
+                    <a class="btn" href="tel:2610001000"><img src="/img/footer/whatsapp.svg" alt="Telefono"
+                            width="30vw"><b>261 010-1010</b></a>
                     <p><strong>Viernes a Domingo</strong><br>17 a 23.30h</p>
-                </article>
-            </div>
+                </li>
+            </ul>
         </section>
-        <section class="__wrapper-copyright">
-            <div class="row __max-width">
-                <div class="col-12 text-center">
-                    <small>© 2019 <strong>Super Mayorista</strong>. Todos los derechos reservados</small>
-                </div>
-            </div>
+        <section class="text-center">
+            <small>© 2019 <strong>Super Mayorista</strong>. Todos los derechos reservados</small>
         </section>
     </footer>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
